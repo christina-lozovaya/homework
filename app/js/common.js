c74ab06aa84310adbb89dd6fe297d850f219f47e
Christina <k7772210@gmail.com>
2019-01-25 17:09:49 +0200

    second save

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,20 @@
+//-----toggle Navigation menu
+
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(500);
+  });//end slide toggle
+  
+  $(window).resize(function() {		
+		if (  $(window).width() > 500 ) {			
+			$('nav ul').removeAttr('style');
+		 }
+	});//end resize
+});//end ready
+
+//---------------------------------------------------------------
+
+
 $(function() {
 
 	// Custom JS
